9a50c6729d8be2e38a8714af46043b870ba61d99
dinghuanghao <aa531811820@gmail.com>
2018-10-13 22:25:59 +0800

    add image downloader

---
+++ util/__init__.py
@@ -0,0 +1,5 @@
+import os
+
+from . import path
+
+path.ROOT_PATH = os.path.abspath("../")
---
+++ util/path.py
@@ -0,0 +1,39 @@
+import os
+import pathlib
+
+import util
+
+# 工程根目录
+ROOT_PATH = os.path.abspath(os.sep.join(util.__file__.split(os.sep)[:-2]))
+
+DATA_PATH = os.path.join(ROOT_PATH, "data")
+IMAGES_PATH = os.path.join(DATA_PATH, "images")
+ORIGINAL_IMAGES_PATH = os.path.join(IMAGES_PATH, "original")
+
+TRAIN_IMAGES_SUBDIR = "train"
+TEST_IMAGES_SUBDIR = "test"
+
+ORIGINAL_TRAIN_IMAGES_PATH = os.path.join(ORIGINAL_IMAGES_PATH, TRAIN_IMAGES_SUBDIR)
+ORIGINAL_TEST_IMAGES_PATH = os.path.join(ORIGINAL_IMAGES_PATH, TEST_IMAGES_SUBDIR)
+
+TXT_PATH = os.path.join(DATA_PATH, "txt")
+TRAIN_DATA_TXT = os.path.join(TXT_PATH, "train-image.txt")
+TEST_DATA_TXT = os.path.join(TXT_PATH, "test-image.txt")
+TEST_RESULT_TXT = os.path.join(TXT_PATH, "test-label.txt")
+
+
+def get_train_data_path(data_type):
+    return os.path.join(os.path.join(IMAGES_PATH, data_type), TRAIN_IMAGES_SUBDIR)
+
+
+def get_test_data_path(data_type):
+    return os.path.join(os.path.join(IMAGES_PATH, data_type), TEST_IMAGES_SUBDIR)
+
+
+def image_path_init():
+    pathlib.Path(ORIGINAL_TRAIN_IMAGES_PATH).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(ORIGINAL_TEST_IMAGES_PATH).mkdir(parents=True, exist_ok=True)
+
+
+# 初始化必要的工程目录
+image_path_init()
